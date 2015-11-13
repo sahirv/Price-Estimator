5a4b298b538ee3cadec0aa203b996aa71e4ee915
sahirv <s3vellan@uwaterloo.ca>
2015-11-12 21:43:53 -0500

    Remove blog folder

--- blog/app/controllers/welcome_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-  end
-  def search
-    priceArray = params[:priceArray]
-    priceArray[0]
-    #average = priceArray.inject{ |sum, el| sum + el }.to_f / priceArray.size
-  end
-end
